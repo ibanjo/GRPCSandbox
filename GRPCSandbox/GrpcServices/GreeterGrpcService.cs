dcf00b94b82a7113f8117e831f0c006467bcfe38
Giuseppe Maria D'Aucelli <giuseppe.daucelli@wolterskluwer.com>
2021-02-12 20:58:49 +0100

    Add project files.

---
+++ GRPCSandbox/GrpcServices/GreeterGrpcService.cs
@@ -0,0 +1,18 @@
+﻿using Grpc.Core;
+using GrpcGreeter;
+using System.Threading.Tasks;
+using static GrpcGreeter.GreeterGrpcService;
+
+namespace GRPCSandbox.GrpcServices
+{
+    public class GreeterGrpcService : GreeterGrpcServiceBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply 
+            { 
+                Message = $"Hello, {request.Name}!"
+            });
+        }
+    }
+}
---
+++ GRPCSandbox/Startup.cs
@@ -0,0 +1,58 @@
+using GRPCSandbox.GrpcServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GRPCSandbox
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddGrpc();
+            services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+            }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            //app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
+            app.UseGrpcWeb();
+            app.UseCors();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGrpcService<GreeterGrpcService>()
+                         .EnableGrpcWeb()
+                         .RequireCors("AllowAll");
+            });
+        }
+    }
+}
